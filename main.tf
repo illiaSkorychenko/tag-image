e5603467d86d2804f604e0cd0a4cf66a33870d15
Illia Skorychenko <i.skorychenko@softonix.org>
2024-10-29 23:39:53 +0200

    remote terraform state

--- main.tf
+++ main.tf
@@ -11,6 +11,52 @@ provider "aws" {
   region = local.region
 }
 
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraform-tag-image-bucket-state"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform_state_locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "terraform-tag-image-bucket-state"
+    key            = "global/s3/terraform.tfstate"
+    region         = "eu-central-1"
+    dynamodb_table = "terraform_state_locks"
+    encrypt        = true
+  }
+}
+
 resource "aws_s3_bucket" "image_bucket" {
   bucket = local.s3_bucket_name
 }

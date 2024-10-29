06709a8f456eddf6bc829f326af4f9c0614d1864
Illia Skorychenko <i.skorychenko@softonix.org>
2024-10-29 22:18:35 +0200

    Refactor getUploadLink function to handle errors and return appropriate response

--- src/get-upload-link/index.ts
+++ src/get-upload-link/index.ts
@@ -6,15 +6,24 @@ import { APIGatewayProxyHandlerV2 } from 'aws-lambda';
 const s3Client = new S3Client({ region: process.env.AWS_REGION });
 
 export const getUploadLink: APIGatewayProxyHandlerV2 = async () => {
-  const uploadId = randomUUID();
-  const command = new PutObjectCommand({
-    Bucket: process.env.BUCKET_NAME,
-    Key: uploadId,
-  });
-  const uploadUrl = await getSignedUrl(s3Client, command, { expiresIn: 3600 });
+  try {
+    const uploadId = randomUUID();
+    const command = new PutObjectCommand({
+      Bucket: process.env.BUCKET_NAME,
+      Key: uploadId,
+    });
+    const uploadUrl = await getSignedUrl(s3Client, command, { expiresIn: 3600 });
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ uploadUrl, uploadId }),
-  };
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ uploadUrl, uploadId }),
+    };
+  } catch (err) {
+    console.error(err);
+
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ message: 'Error getting upload link' }),
+    };
+  }
 };
